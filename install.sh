1475d17718e9b057eb40c5dee698bdedfcd99d5c
TrinityCoder <mmares.trinity@gmail.com>
2020-12-22 15:24:43 +0100

    package.json: Removed `scripts` - `install.sh` does all
    End of `package.json` scripts experiment - it is simpler
    and more effective to just use `install.sh` for everything.

--- install.sh
+++ install.sh
@@ -3,23 +3,25 @@
 set -o errexit
 set -o xtrace
 
+# Returns "true" <==> "$1" package is installed in Atom.
 is_installed() {
-  (( $# >= 1)) &&
-    apm list | grep "$@" || { return 1; }
+  (( $# == 1)) && apm list | grep "$@"
+}
+
+# Uninstalls all "$@" packages, which are installed.
+uninstall_if_installed() {
+  for NAME in "$@"; do
+    is_installed "$NAME" && apm uninstall --hard "$NAME"
+  done
 }
 
 # 0a) Get rid of `language-cmake` and `language-cmake-2` as well.
-is_installed "language-cmake@" &&
-  apm uninstall --hard language-cmake
-is_installed "language-cmake-2" &&
-  apm uninstall --hard language-cmake-2
+uninstall_if_installed "language-cmake@" "language-cmake-2"
 
 # 1) If `--link` is specified, link current directory into Atom
 #    as the current `language-cmake-2` version.
 #
 #    Otherwise, install the last officially published `language-cmake-2`.
-[[ "$1" = "--link" ]] && {
-  apm link --no-dev
-} || {
+[[ "$1" = "--link" ]] &&
+  apm link --no-dev ||
   apm install language-cmake-2
-}
