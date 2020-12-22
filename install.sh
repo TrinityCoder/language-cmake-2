4fa62fe41b1e223af6e6956b3f7447567b036023
TrinityCoder <mmares.trinity@gmail.com>
2020-12-22 22:30:40 +0100

    Added over 25 upper-case constants
    Such as:
    * LANGUAGE
    * CMAKE_OBJC_EXTENSIONS
    * OBJC_EXTENSIONS
    * OBJC_STANDARD
    * CMAKE_OBJCXX_STANDARD_REQUIRED
    * CMAKE_OBJCXX_STANDARD
    * CMAKE_OBJC_STANDARD
    * UNITY_BUILD_MODE
    * UNITY_BUILD_UNIQUE_ID
    * UNITY_GROUP
    * CMAKE_OSX_SYSROOT
    * CMAKE_OSX_DEPLOYMENT_TARGET
    * CMAKE_OSX_ARCHITECTURES
    * POSITION_INDEPENDENT_CODE
    and so on.

--- install.sh
+++ install.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 set -o errexit
-set -o xtrace
 
 # Returns "true" <==> "$1" package is installed in Atom.
-is_installed() {
+apm_installed() {
   (( $# == 1)) && apm list | grep "$@"
 }
 
 # Uninstalls all "$@" packages, which are installed.
-uninstall_if_installed() {
+apm_uninstall() {
   for NAME in "$@"; do
-    is_installed "$NAME" && apm uninstall --hard "$NAME"
+    apm_installed "$NAME" && apm uninstall --hard "$NAME"
   done
 }
 
 # 0a) Get rid of `language-cmake` and `language-cmake-2` as well.
-uninstall_if_installed "language-cmake@" "language-cmake-2"
+apm_uninstall "language-cmake@" "language-cmake-2"
 
-# 1) If `--link` is specified, link current directory into Atom
-#    as the current `language-cmake-2` version.
-#
-#    Otherwise, install the last officially published `language-cmake-2`.
+# 1) Install the `language-cmake-2` package into Atom.
+# If `--link` arg is specified, the current directory's package
+# gets linked into Atom.
+# Otherwise, the newest officially published version is downloaded
+# and installed.
 [[ "$1" = "--link" ]] &&
   apm link --no-dev ||
   apm install language-cmake-2
